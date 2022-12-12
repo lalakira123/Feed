06d53d04e97b93f99b30504f6fa009975efe71e3
Laerte <laerteakira@gmail.com>
2022-12-12 14:02:02 -0300

    feat: SideBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import { Header } from './components/Header/Header'
+import { Aside } from './components/Aside/Aside'
+
+import style from './App.module.css'
 
 function App() {
   return (
     <>
       <Header />
+
+      <div className={style.wrapper}>
+        <Aside/>
+        <main>b</main>
+      </div>
     </>
   )
 }
---
+++ src/components/Aside/Aside.jsx
@@ -0,0 +1,30 @@
+import { PencilLine } from 'phosphor-react'
+import style from './Aside.module.css'
+
+export function Aside() {
+  return(
+    <aside className={style.aside}>
+			<img 
+				className={style.cover}
+				src='https://images.unsplash.com/photo-1484417894907-623942c8ee29?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=80'
+				alt='Capa'
+			/>
+
+			<div className={style.profile}>
+				<img
+					src='https://github.com/lalakira123.png'
+					alt='Foto Perfil'
+				/>
+				<strong>Laerte Akira</strong>
+				<span>Developer Full Stack</span>
+			</div>
+
+			<footer>
+				<a href='#'>
+					<PencilLine/>
+					Editar seu perfil
+				</a>
+			</footer>
+    </aside>
+  );
+}
